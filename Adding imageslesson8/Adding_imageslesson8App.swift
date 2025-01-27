6c9bbfaf1adf90108c79c76cd7ec446506e1e790
adina778899 <adinamemetea@yahoo.com>
2025-01-26 19:50:47 -0600

    Initial Commit

---
+++ Adding imageslesson8/Adding_imageslesson8App.swift
@@ -0,0 +1,17 @@
+//
+//  Adding_imageslesson8App.swift
+//  Adding imageslesson8
+//
+//  Created by natural health on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Adding_imageslesson8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adding imageslesson8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Adding imageslesson8
+//
+//  Created by natural health on 1/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
